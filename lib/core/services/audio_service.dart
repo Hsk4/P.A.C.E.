72d501d2c69474d0f0167a3cd3442594a06e0474
Hsk4 <hsk42116@gmail.com>
2026-05-22 22:16:39 +0500

    v1 complete

--- lib/core/services/audio_service.dart
+++ lib/core/services/audio_service.dart
@@ -1,19 +1,6 @@
-import 'package:audioplayers/audioplayers.dart';
-
 class AudioService {
-  static final AudioPlayer _audioPlayer = AudioPlayer();
-
-  static Future<void> playCustomTone (String path) async{
-    await _audioPlayer.stop();
-    await _audioPlayer.play(DeviceFileSource(path));
-  }
-
-  static Future<void> playAssetTone(String assetPath) async{
-    await _audioPlayer.stop();
-    await _audioPlayer.play(AssetSource(assetPath));
-  }
-
-  static Future<void> stopTone() async{
-    await _audioPlayer.stop();
-  }
+  // No-op placeholder kept for future tone playback integration.
+  static Future<void> playCustomTone(String path) async {}
+  static Future<void> playAssetTone(String assetPath) async {}
+  static Future<void> stopTone() async {}
 }
